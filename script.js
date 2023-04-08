eafc1b02683336879e22822a8ca4c9c6068ca0c1
Davi Brian <davipatino2009@gmail.com>
2023-04-08 12:50:44 -0300

    Damage Calculator v1.2

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
+const result = document.getElementById('result');
+let final=0;let damage=0;let strength=0;let crit=0;let post=0;let enchants = 0;let ent= 0;let sharp = 0;
+
+function ofa(){
+    if(ent==0){
+        enchants = 500;
+        ent = 1;
+    }else if(ent == 1){
+        enchants = enchants - 500;
+        ent=0;
+    }
+}
+
 function calculo(){
-    let final = 0;let damage = document.querySelector('input#damage');let strength = document.querySelector('input#strength');let crit = document.querySelector('input#crit');let base = 0; let post = 0;
-    const result = document.getElementById('result');
-    final = ((5+Number(damage.value))*(1+(Number(strength.value)/100))*(1+(Number(crit.value)/100))*(1+(base/100))*(1+(post/100))).toFixed(0);
+    final = 0;damage = document.querySelector('input#damage');strength = document.querySelector('input#strength');crit = document.querySelector('input#crit');post = 0;sharp = document.querySelector('input#sharp');
+    final = ((5+Number(damage.value))*(1+(Number(strength.value)/100))*(1+(Number(crit.value)/100))*(1+(enchants+(Number(sharp.value)*5)/100))*(1+(post/100))).toFixed(0);
     result.innerHTML = `Your Damage is: ${final}`
 }
+
